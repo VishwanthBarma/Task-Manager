0d330daf2672a990ffbccdd075356a00da57f159
Barma Vishwanth <vishwanthbsavp@gmail.com>
2022-06-02 23:33:37 +0530

    code-first graphql apis using nexus

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,54 +1,71 @@
-import { gql } from 'apollo-server-micro';
+import { makeSchema } from "nexus";
+import { join } from "path";
 
-export const typeDefs = gql `
-    scalar Date
+export const schema = makeSchema({
+    types: [],
+    outputs: {
+      typegen: join(process.cwd(), 'node_modules', '@types', 'nexus-typegen', 'index.d.ts'),
+      schema: join(process.cwd(), 'graphql', 'schema.graphql'),
+    },
+    contextType: {
+      export: 'Context',
+      module: join(process.cwd(), 'graphql', 'context.ts'),
+    },
+})
 
-    type User {
-        id: Int
-        email: String
-        name: String
-        username: String
-        image: String
-        notes: [Note]
-    }
 
-    type Note {
-        id: Int
-        createdAt: Date
-        updatedAt: Date
-        userId: Int
-        user: User
-        content: String
-        tag: Tag
-        detailNote: DetailNote
-    }
+// import { gql } from 'apollo-server-micro';
 
-    type DetailNote {
-        id: Int
-        noteId: Int
-        note: Note
-        detailContent: String
-    }
 
-    enum Tag {
-        STARTED
-        INPROGRESS
-        PENDING
-        COMPLETED
-    }
+// export const typeDefs = gql `
+//     scalar Date
 
-    type Query {
-        user(id: Int!): User!
-        notes: [Note]!
-        note(id: Int!): Note!
-        detailNote(id: Int!): DetailNote!
-    }
+//     type User {
+//         id: Int
+//         email: String
+//         name: String
+//         username: String
+//         image: String
+//         notes: [Note]
+//     }
 
-    type Mutation {
-        createUser(email: String!, name: String!, username: String!, image: String!) : User!
-        createNote(userId: Int!, content: String, tag: Tag!): Note!
-        createDetailNote(noteId: Int!, detailContent: String!): DetailNote!
-        deleteNote(id: Int!, userId: Int!): Note!
-        deleteDetailNote(id: Int!, noteId: Int!): DetailNote!
-    }
-`;
+//     type Note {
+//         id: Int
+//         createdAt: Date
+//         updatedAt: Date
+//         userId: Int
+//         user: User
+//         content: String
+//         tag: Tag
+//         detailNote: DetailNote
+//     }
+
+//     type DetailNote {
+//         id: Int
+//         noteId: Int
+//         note: Note
+//         detailContent: String
+//     }
+
+//     enum Tag {
+//         STARTED
+//         INPROGRESS
+//         PENDING
+//         COMPLETED
+//     }
+
+//     type Query {
+//         user(id: Int!): User!
+//         notes: [Note]!
+//         note(id: Int!): Note!
+//         detailNote(id: Int!): DetailNote!
+//     }
+
+//     type Mutation {
+//         createUser(email: String!, name: String!, username: String!, image: String!) : User!
+//         createNote(userId: Int!, content: String, tag: Tag!): Note!
+//         createDetailNote(noteId: Int!, detailContent: String!): DetailNote!
+//         deleteNote(id: Int!, userId: Int!): Note!
+//         deleteDetailNote(id: Int!, noteId: Int!): DetailNote!
+//     }
+// `;
